c088a5b11adab5d0cfd35ed639320411d31ba747
cristianbiancalana <criseze156@gmail.com>
2024-06-17 08:53:46 -0300

    Added logic of personajes.js for load data of API the Harry Potter

--- js/personajes.js
+++ js/personajes.js
@@ -1,4 +1,6 @@
 const $divc = document.getElementById("grid");
+let numeracion = 1;
+
 
 fetch('https://hp-api.onrender.com/api/characters')
     .then((resp) => {
@@ -6,11 +8,43 @@ fetch('https://hp-api.onrender.com/api/characters')
     }).then((characters) => {
         console.log(characters);
         characters.forEach(character => {
+            let img = character.image;
+            const alive = character.alive;
+            const gender = character.gener;
+            if (img === "") {
+                img = "../assets/img/photonotfound.webp";
+            }
+            if (alive) {
+                vive = "Si, aún vive ";
+            } else {
+                vive = "No :(";
+            }
+            if (gender==="male") {
+                ocupation = "Actor";
+            } else{
+                ocupation = "Actriz";
+            }
             $divc.innerHTML += `
-                <div>
-                    <h4>${character.name}</h4>
+            <div class="card" id="card">
+                <div class="header-card">
+                    <span> <strong> # ${numeracion}</strong> </span>
+                    <h3> ${character.name} </h3>
                 </div>
-            
+                <div class="img-card">
+                    <img src="${img}" alt="${character.name}">
+                </div>
+                <div class="details-card">
+                    <ul>
+                    <li>Especie: ${character.species}</li>
+                    <li>Casa: ${character.house}</li>
+                    <li>Patronus: ${character.patronus}</li>
+                    <li>Se encuentra vivo? ${vive}</li>
+                    <li>${ocupation}: ${character.actor} </li>
+                    <li>Cumpleaños: ${character.dateOfBirth}</li>
+                    </ul>
+                </div>
+            </div>
             `;
+            numeracion = numeracion + 1;
         });
     });
